f9a4726104d3ed9f95936ca66f08b6efbd9c592a
Dominik Wójt <domin144@o2.pl>
2018-05-23 11:00:51 +0200

    added license

--- formal_av1.v
+++ formal_av1.v
@@ -1,3 +1,23 @@
+(* 
+ * Copyright 2018 Dominik Wójt
+ * 
+ * This file is part of formal_av1.
+ * 
+ * formal_av1 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * formal_av1 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with formal_av1.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *)
+
 Require Import Bool Arith List.
 Import ListNotations.
 Require Import String.
@@ -4217,19 +4237,5 @@ decode_subexp_bool( numSyms, k ) {
 }
 ".
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+End av1.
 
