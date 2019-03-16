0a9186a9c42ee6d99021374c7af6d4ffed8576ed
Dominik Wójt <domin144@o2.pl>
2019-03-16 15:34:54 +0100

    syntax_processor, eval_expr: operators with no side effects

--- syntax_processor.v
+++ syntax_processor.v
@@ -28,13 +28,18 @@ Inductive expr_result : Type :=
   | er_value : Z -> expr_result
   | er_reference : reference -> expr_result.
 
+Fixpoint binary_negation (x : Z) : Z := -x - 1.
+
 Inductive eval_op1_no_side_effect : any_operator -> Z -> Z -> Prop :=
   | eval_op1_minus_unary :
     forall x,
       eval_op1_no_side_effect (ano_ao ao_minus_unary) x (-x)
   | eval_op1_lo_not :
     forall x,
-      eval_op1_no_side_effect (ano_lo lo_not) x (match x with | 0%Z => 1%Z | _ => 0%Z end).
+      eval_op1_no_side_effect (ano_lo lo_not) x (match x with | 0%Z => 1%Z | _ => 0%Z end)
+  | eval_op1_bo_neg :
+    forall x,
+      eval_op1_no_side_effect (ano_bo bo_neg) x (binary_negation x).
 
 (* TODO: bitwise operators *)
 Inductive eval_op2_no_side_effect : any_operator -> Z -> Z -> Z -> Prop :=
@@ -87,7 +92,24 @@ Inductive eval_op2_no_side_effect : any_operator -> Z -> Z -> Z -> Prop :=
       eval_op2_no_side_effect (ano_ro ro_eq) x0 x1 (if Z.eqb x0 x1 then 1%Z else 0%Z)
   | eval_op2_neq :
     forall x0 x1,
-      eval_op2_no_side_effect (ano_ro ro_gt) x0 x1 (if Z.eqb x0 x1 then 0%Z else 1%Z).
+      eval_op2_no_side_effect (ano_ro ro_gt) x0 x1 (if Z.eqb x0 x1 then 0%Z else 1%Z)
+  | eval_op2_bo_and :
+    forall x0 x1,
+      eval_op2_no_side_effect (ano_bo bo_and) x0 x1 (Z.land x0 x1)
+  | eval_op2_bo_or :
+    forall x0 x1,
+      eval_op2_no_side_effect (ano_bo bo_or) x0 x1 (Z.lor x0 x1)
+  | eval_op2_bo_xor :
+    forall x0 x1,
+      eval_op2_no_side_effect (ano_bo bo_xor) x0 x1 (Z.lxor x0 x1)
+  | eval_op2_bo_rshift :
+    forall x0 x1,
+      (x1 >= 0)%Z ->
+        eval_op2_no_side_effect (ano_bo bo_rshift) x0 x1 (Z.shiftr x0 x1)
+  | eval_op2_bo_lshift :
+    forall x0 x1,
+      (x1 >= 0)%Z ->
+        eval_op2_no_side_effect (ano_bo bo_lshift) x0 x1 (Z.shiftl x0 x1).
 
 (* TODO: add more rules *)
 (* Note: order of evaluation of subexpressions is fixed:
@@ -99,11 +121,17 @@ Inductive eval_expr : expression -> state -> state -> expr_result -> Prop :=
   | eval_expr_variable :
     forall st label,
       expr_variable label // st \\ st \\ er_reference (ref_variable label [])
-  | eval_expr_plus :
-    forall e0 e1 x0 x1 st0 st1 st2,
+  | eval_expr_op1_no_side_effect :
+    forall e x y st0 st1 op,
+      e // st0 \\ st1 \\ er_value x ->
+        eval_op1_no_side_effect op x y ->
+          expr_op1 op e // st0 \\ st1 \\ er_value y
+  | eval_expr_op2_no_side_effect :
+    forall e0 e1 x0 x1 y st0 st1 st2 op,
       e0 // st0 \\ st1 \\ er_value x0 ->
         e1 // st1 \\ st2 \\ er_value x1 ->
-          expr_op2 (ano_ao ao_plus) e0 e1 // st0 \\ st2 \\ er_value (x0 + x1)
+          eval_op2_no_side_effect op x0 x1 y ->
+            expr_op2 (ano_ao ao_plus) e0 e1 // st0 \\ st2 \\ er_value y
   | eval_expr_reference :
     forall st ref x e,
       st ref = Some x ->
