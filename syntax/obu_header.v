150873de9ae0cfd0ec4d1b0d16e32ec2ef4e7fed
Dominik Wójt <domin144@o2.pl>
2023-01-18 17:33:35 +0100

    move obu_header_type to structures.v

--- syntax/obu_header.v
+++ syntax/obu_header.v
@@ -1,9 +1,11 @@
 Require Import Fin.
 Require Import List.
 Require Import ZArith.
+Import ListNotations.
+
 Require Import formal_av1.basic_types.
 Require Import formal_av1.entropy.descriptors.
-Import ListNotations.
+Require formal_av1.structures.
 
 Inductive conditional_decode_relation
     {X : Set}
@@ -30,77 +32,7 @@ Inductive conditional_decode_relation
         []
         default_value.
 
-(* TODO: move enums somewhere else *)
-Module obu_type_enum.
-
-  Inductive t : Set :=
-    | Reserved_0
-    | OBU_SEQUENCE_HEADER
-    | OBU_TEMPORAL_DELIMITER
-    | OBU_FRAME_HEADER
-    | OBU_TILE_GROUP
-    | OBU_METADATA
-    | OBU_FRAME
-    | OBU_REDUNDANT_FRAME_HEADER
-    | OBU_TILE_LIST
-    | Reserved_9
-    | Reserved_10
-    | Reserved_11
-    | Reserved_12
-    | Reserved_13
-    | Reserved_14
-    | OBU_PADDING.
-
-  Definition to_nat (x : t) : nat :=
-      match x with
-      | Reserved_0                  =>  0
-      | OBU_SEQUENCE_HEADER         =>  1
-      | OBU_TEMPORAL_DELIMITER      =>  2
-      | OBU_FRAME_HEADER            =>  3
-      | OBU_TILE_GROUP              =>  4
-      | OBU_METADATA                =>  5
-      | OBU_FRAME                   =>  6
-      | OBU_REDUNDANT_FRAME_HEADER  =>  7
-      | OBU_TILE_LIST               =>  8
-      | Reserved_9                  =>  9
-      | Reserved_10                 => 10
-      | Reserved_11                 => 11
-      | Reserved_12                 => 12
-      | Reserved_13                 => 13
-      | Reserved_14                 => 14
-      | OBU_PADDING                 => 15
-      end.
-
-End obu_type_enum.
-
-(* Definition fin_by_bits (n : nat) : Set :=
-  Fin.t (2 ^ n).
-
-Record obu_header : Set := make_obu_header
-{
-  obu_forbidden_bit : fin_by_bits 1;
-  obu_type : obu_type_enum.t;
-  obu_extension_flag : fin_by_bits 1;
-  obu_has_size_field : bool;
-  obu_reserved_1bit : fin_by_bits 1;
-  temporal_id : fin_by_bits 3;
-  spatial_id : fin_by_bits 2;
-  extension_header_reserved_3bits : fin_by_bits 3
-}. *)
-
-Record obu_header_type : Set := make_obu_header
-{
-  obu_forbidden_bit : nat;
-  obu_type : obu_type_enum.t;
-  obu_extension_flag : bool;
-  obu_has_size_field : bool;
-  obu_reserved_1bit : nat;
-  temporal_id : nat;
-  spatial_id : nat;
-  extension_header_reserved_3bits : nat
-}.
-
-Inductive obu_header_decode_relation : list bit -> obu_header_type -> Prop :=
+Inductive obu_header_decode_relation : list bit -> structures.obu_header_type -> Prop :=
   obu_header_decode_intro :
     forall
         obu_forbidden_bit_bits
@@ -126,7 +58,7 @@ Inductive obu_header_decode_relation : list bit -> obu_header_type -> Prop :=
     f_decode_relation
         3
         obu_type_bits
-        (obu_type_enum.to_nat obu_type_value) ->
+        (structures.obu_type_enum.to_nat obu_type_value) ->
     f_decode_relation
         1
         obu_extension_flag_bits
@@ -168,7 +100,7 @@ Inductive obu_header_decode_relation : list bit -> obu_header_type -> Prop :=
           ++ spatial_id_bits
           ++ extension_header_reserved_3bits_bits)
         (
-          make_obu_header
+          structures.make_obu_header
               obu_forbidden_bit_value
               obu_type_value
               obu_extension_flag_value
--- syntax/open_bitstream_unit.v
+++ syntax/open_bitstream_unit.v
@@ -2,6 +2,7 @@ Require Import List.
 Require Import ZArith.
 Require Import formal_av1.basic_types.
 Require Import formal_av1.syntax.obu_header.
+Require formal_av1.structures.
 Import ListNotations.
 
 Record sequence_header_obu_type : Set := make_sequence_header_obu
@@ -23,28 +24,28 @@ Record padding_obu_type : Set := make_padding_obu
 Record reserved_obu_type : Set := make_reserved_obu
 { (* TODO *) }.
 
-Definition obu_type_to_payload_type (type : obu_type_enum.t) : Set :=
+Definition obu_type_to_payload_type (type : structures.obu_type_enum.t) : Set :=
   match type with
-  | obu_type_enum.Reserved_0 => reserved_obu_type
-  | obu_type_enum.OBU_SEQUENCE_HEADER => sequence_header_obu_type
-  | obu_type_enum.OBU_TEMPORAL_DELIMITER => temporal_delimiter_obu_type
-  | obu_type_enum.OBU_FRAME_HEADER => frame_header_obu_type
-  | obu_type_enum.OBU_TILE_GROUP => tile_group_obu_type
-  | obu_type_enum.OBU_METADATA => metadata_obu_type
-  | obu_type_enum.OBU_FRAME => frame_obu_type
-  | obu_type_enum.OBU_REDUNDANT_FRAME_HEADER => frame_header_obu_type
-  | obu_type_enum.OBU_TILE_LIST => tile_list_obu_type
-  | obu_type_enum.Reserved_9 => reserved_obu_type
-  | obu_type_enum.Reserved_10 => reserved_obu_type
-  | obu_type_enum.Reserved_11 => reserved_obu_type
-  | obu_type_enum.Reserved_12 => reserved_obu_type
-  | obu_type_enum.Reserved_13 => reserved_obu_type
-  | obu_type_enum.Reserved_14 => reserved_obu_type
-  | obu_type_enum.OBU_PADDING => padding_obu_type
+  | structures.obu_type_enum.Reserved_0 => reserved_obu_type
+  | structures.obu_type_enum.OBU_SEQUENCE_HEADER => sequence_header_obu_type
+  | structures.obu_type_enum.OBU_TEMPORAL_DELIMITER => temporal_delimiter_obu_type
+  | structures.obu_type_enum.OBU_FRAME_HEADER => frame_header_obu_type
+  | structures.obu_type_enum.OBU_TILE_GROUP => tile_group_obu_type
+  | structures.obu_type_enum.OBU_METADATA => metadata_obu_type
+  | structures.obu_type_enum.OBU_FRAME => frame_obu_type
+  | structures.obu_type_enum.OBU_REDUNDANT_FRAME_HEADER => frame_header_obu_type
+  | structures.obu_type_enum.OBU_TILE_LIST => tile_list_obu_type
+  | structures.obu_type_enum.Reserved_9 => reserved_obu_type
+  | structures.obu_type_enum.Reserved_10 => reserved_obu_type
+  | structures.obu_type_enum.Reserved_11 => reserved_obu_type
+  | structures.obu_type_enum.Reserved_12 => reserved_obu_type
+  | structures.obu_type_enum.Reserved_13 => reserved_obu_type
+  | structures.obu_type_enum.Reserved_14 => reserved_obu_type
+  | structures.obu_type_enum.OBU_PADDING => padding_obu_type
   end.
 
 Record open_bitstream_unit_type : Set := make_open_bitstream_unit
 {
-  obu_header : obu_header_type;
-  obu_payload : option (obu_type_to_payload_type (obu_type obu_header))
+  obu_header : structures.obu_header_type;
+  obu_payload : obu_type_to_payload_type (structures.obu_type obu_header)
 }.
